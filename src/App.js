a02ff0d1fd3bf208614ec89637a70150e60cdbb4
Srikanth Marrapu <srikanth23111995@gmail.com>
2021-09-24 03:45:37 +0530

    Initial Commit with Alan Integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React,{useEffect} from 'react';
 import './App.css';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=() =>{
+  const alanKey='59bbc8c3150b7e064ceabd6ee5cbe28c2e956eca572e1d8b807a3e2338fdd0dc/stage'
+  useEffect(()=>{
+    alanBtn({
+      key:alanKey,
+      onCommand:({command})=>{
+        if(command==='testCommand'){
+          alert('This code was executed successfully')
+        }
+      }
+    })
+  },[])
+
+  return ( 
+
+    <div>
+      <h1>Alan AI News Application</h1>
     </div>
+    
   );
-}
+}   
 
 export default App;
