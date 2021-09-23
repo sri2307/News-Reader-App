2957902b8c71b69240b89924c9de9517cfd80467
Srikanth Marrapu <srikanth23111995@gmail.com>
2021-09-24 05:01:00 +0530

    added news api to alan

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React,{useEffect} from 'react';
+import React, { useState,useEffect } from 'react';
 import './App.css';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
-const App=() =>{
-  const alanKey='59bbc8c3150b7e064ceabd6ee5cbe28c2e956eca572e1d8b807a3e2338fdd0dc/stage'
-  useEffect(()=>{
+const App = () => {
+  const alanKey = '59bbc8c3150b7e064ceabd6ee5cbe28c2e956eca572e1d8b807a3e2338fdd0dc/stage';
+  const [newsArticles,setNewsArticles]=useState([])
+  useEffect(() => {
     alanBtn({
-      key:alanKey,
-      onCommand:({command})=>{
-        if(command==='testCommand'){
-          alert('This code was executed successfully')
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
         }
       }
     })
-  },[])
+  }, [])
 
-  return ( 
+  return (
 
     <div>
       <h1>Alan AI News Application</h1>
     </div>
-    
+
   );
-}   
+}
 
 export default App;
