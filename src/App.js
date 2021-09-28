495d2c189db503800434f8d1aa7fc420417332ef
Srikanth Marrapu <srikanth23111995@gmail.com>
2021-09-29 03:33:34 +0530

    Final Draft

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
-import React, { useState,useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
+import git from './images/git.jpg';
+import ld from './images/ld.jpg';
+import twitter from './images/twitter.jpg';
+import { NewsCards, Modal } from './components';
+import useStyles from './styles';
+
 const App = () => {
-  const alanKey = '59bbc8c3150b7e064ceabd6ee5cbe28c2e956eca572e1d8b807a3e2338fdd0dc/stage';
-  const [newsArticles,setNewsArticles]=useState([])
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
+
   useEffect(() => {
     alanBtn({
-      key: alanKey,
-      onCommand: ({ command, articles }) => {
+      key: '64370f4c903e66c5b517887fefa45c1b2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, articles, number }) => {
         if (command === 'newHeadlines') {
           setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'instructions') {
+          setIsOpen(true);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
         }
-      }
-    })
-  }, [])
+      },
+    });
+  }, []);
 
   return (
-
     <div>
-      <h1>Alan AI News Application</h1>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+        <img src="https://images.prismic.io/ionicframeworkcom/668a7aa44c002ae8e2582d772b6f0cc03af90f17_alan-icon.png?auto=compress,format" className={classes.alanLogo} alt="logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+         
+          <img className={classes.image} src={git} height="50px" alt="Sri logo" />
+          <img className={classes.image} src={ld} height="50px" alt="Linkedin logo" />
+          <img className={classes.image} src={twitter} height="50px" alt="Sri logo" />
+        </div>
+      ) : null}
     </div>
-
   );
-}
+};
 
 export default App;
